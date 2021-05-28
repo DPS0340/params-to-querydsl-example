532e04124a44220865f64ddf4e3bdf8940fd3366
jiho lee <jhlee@autocrypt.io>
2021-05-28 23:28:05 +0900

    [feat/front] 엔티티 생성

---
+++ src/main/java/generated/io/github/dps0340/paramstoquerydslexample/entity/QCar.java
@@ -0,0 +1,55 @@
+package io.github.dps0340.paramstoquerydslexample.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QCar is a Querydsl query type for Car
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QCar extends EntityPathBase<Car> {
+
+    private static final long serialVersionUID = -904289394L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QCar car = new QCar("car");
+
+    public final QCompany company;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public final StringPath number = createString("number");
+
+    public QCar(String variable) {
+        this(Car.class, forVariable(variable), INITS);
+    }
+
+    public QCar(Path<? extends Car> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QCar(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QCar(PathMetadata metadata, PathInits inits) {
+        this(Car.class, metadata, inits);
+    }
+
+    public QCar(Class<? extends Car> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.company = inits.isInitialized("company") ? new QCompany(forProperty("company")) : null;
+    }
+
+}
+
---
+++ src/main/java/io/github/dps0340/paramstoquerydslexample/entity/Car.java
@@ -0,0 +1,25 @@
+package io.github.dps0340.paramstoquerydslexample.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToOne(targetEntity = Company.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "company_id")
+    private Company company;
+    private String number;
+}
