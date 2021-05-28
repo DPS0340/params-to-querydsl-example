5e2f4495f3648e8552d8513ae35ba0836e0ea8b7
jiho lee <jhlee@autocrypt.io>
2021-05-28 23:29:07 +0900

    [feat/front] 애플리케이션 생성

--- src/main/java/io/github/dps0340/paramstoquerydslexample/Application.java
+++ src/main/java/io/github/dps0340/paramstoquerydslexample/Application.java
@@ -1,13 +1,41 @@
 package io.github.dps0340.paramstoquerydslexample;
 
+import io.github.dps0340.paramstoquerydslexample.entity.Car;
+import io.github.dps0340.paramstoquerydslexample.entity.Company;
+import io.github.dps0340.paramstoquerydslexample.repository.CarRepository;
+import io.github.dps0340.paramstoquerydslexample.repository.CompanyRepository;
+import io.reactivex.rxjava3.core.Observable;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private CarRepository carRepository;
+	@Autowired
+	private CompanyRepository companyRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Observable<String> companyNames = Observable.just("현대", "기아", "아우디", "벤츠", "BMW", "테슬라");
+		Observable<Company> companyObservable = companyNames.map(name -> Company.builder().name(name).build());
+
+		companyObservable.forEach(companyRepository::save);
+
+		Observable<String> carNames = Observable.just("쏘나타", "K5", "A8", "S클래스", "i8", "모델 X");
+		Observable<Car> carObservable = carNames.zipWith(companyObservable, (name, company) -> Car.builder().name(name).company(company).build());
+
+		carObservable.forEach(carRepository::save);
+	}
 }
